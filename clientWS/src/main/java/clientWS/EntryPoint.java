f49ca4ca418b9105ff3fb57837f01ce810d16792
training <training@training.vm>
2019-06-19 10:09:35 +0200

    first commit client

---
+++ clientWS/src/main/java/clientWS/EntryPoint.java
@@ -0,0 +1,13 @@
+package clientWS;
+
+import fr.formation.client.ping.Ping;
+import fr.formation.client.ping.User;
+import fr.formation.client.ping.Users_Service;
+
+public class EntryPoint {
+
+	public static void main(String... args) {
+		
+	}
+
+}
